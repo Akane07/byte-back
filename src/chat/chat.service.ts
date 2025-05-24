636ad817182f1299197afcb4afce40f9bfca1d6c
Akane07 <Oreki05@mail.ru>
2025-05-25 01:33:20 +0300

    chat

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -33,7 +33,89 @@ export class ChatService {
     }
 
     async getUserChats(userId: string) {
-        return this.messageModel.find({ $or: [{ senderId: userId }, { receiverId: userId }] }).sort({ createdAt: 1 }); // сортировка по времени
-    }   
+        const chats = await this.messageModel.aggregate([
+            {
+                $match: {
+                    $or: [
+                        { senderId: userId },
+                        { receiverId: userId }
+                    ]
+                }
+            },
+            {
+                $addFields: {
+                    otherUserId: {
+                        $cond: [
+                            { $eq: ['$senderId', userId] },
+                            '$receiverId',
+                            '$senderId'
+                        ]
+                    },
+                    isOwnMessage: { $eq: ['$senderId', userId] },
+                    displayText: {
+                        $cond: [
+                            { $eq: ['$mediaType', 'video'] },
+                            'Видео',
+                            '$text'
+                        ]
+                    }
+                }
+            },
+            { $sort: { createdAt: -1 } },
+            {
+                $group: {
+                    _id: '$otherUserId',
+                    lastMessage: { $first: '$displayText' },
+                    lastMessageDate: { $first: '$createdAt' },
+                    isRead: {
+                        $first: {
+                            $cond: [
+                                { $eq: ['$senderId', userId] },
+                                '$isRead',
+                                '$$REMOVE'
+                            ]
+                        }
+                    }
+                }
+            },
+            {
+                // преобразуем строку в ObjectId
+                $addFields: {
+                    objectOtherUserId: {
+                        $convert: {
+                            input: '$_id',
+                            to: 'objectId',
+                            onError: null,
+                            onNull: null
+                        }
+                    }
+                }
+            },
+            {
+                $lookup: {
+                    from: 'users',
+                    localField: 'objectOtherUserId',
+                    foreignField: '_id',
+                    as: 'user'
+                }
+            },
+            { $unwind: '$user' },
+            {
+                $project: {
+                    _id: 0,
+                    userId: '$_id',
+                    name: '$user.name',
+                    avatar: '$user.avatar',
+                    lastMessage: 1,
+                    lastMessageDate: 1,
+                    isRead: 1
+                }
+            },
+            { $sort: { lastMessageDate: -1 } }
+        ]);
+
+        return chats;
+    }
+
 
 }
